8a90b520018dbe4a6607f86bd39e54a82a1472a7
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-03-16 18:25:22 +0530

    added css and js files for bootstrap

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,13 @@ module Emr
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # config.assets.compress = true 
+    # config.assets.js_compressor = :uglifier
+    # # Version of your assets, change this if you want to expire all your assets
+    # config.assets.version = '1.0'
+
+
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
