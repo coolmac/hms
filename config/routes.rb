14a1f285d5ca55a838a4859a1f822149dbd9bc52
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-03-16 12:02:58 +0530

    adding devise to the rails application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Emr::Application.routes.draw do
+  devise_for :users
+  # devise_for :users, :controllers =>{:sessions => 'sessions', :registrations => 'registrations'},:path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock'}
   resources :users
 
 
