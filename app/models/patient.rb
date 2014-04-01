250826debf2fd801f0941e74a350e67b80c3f239
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-04-01 23:50:38 +0530

    adding more fields to patient registration page

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ActiveRecord::Base
-  attr_accessible :mobile, :name, :husband_name, :father_name, :age, :gender, :aadhaar_card_number, :date_of_visit, :email
+  attr_accessible :mobile, :name, :husband_name, :father_name, :age, :gender, :aadhaar_card_number, :email, :date_of_visit, :date_of_birth, :reference_mobile, :reference_name, :emergency_mobile, :emergency_name, :nationality, :occupation, :patient_type, :id_type, :id_type, :id_number, :referred_by, :insurance_provider, :insurance_policy_number
 
   fuzzily_searchable :name
   fuzzily_searchable :mobile
---
+++ db/migrate/20140401180128_add_more_columns_to_patients.rb
@@ -0,0 +1,18 @@
+class AddMoreColumnsToPatients < ActiveRecord::Migration
+  def change
+  	rename_column :patients, :date_of_visit, :date_of_registration
+  	add_column :patients, :date_of_birth, :datetime
+  	add_column :patients, :reference_mobile, :string
+  	add_column :patients, :reference_name, :string
+  	add_column :patients, :emergency_mobile, :string
+  	add_column :patients, :emergency_name, :string
+  	add_column :patients, :nationality, :string
+  	add_column :patients, :occupation, :string
+  	add_column :patients, :patient_type, :string
+  	add_column :patients, :id_type, :string
+  	add_column :patients, :id_number, :string
+  	add_column :patients, :referred_by, :string
+  	add_column :patients, :insurance_provider, :string
+  	add_column :patients, :insurance_policy_number
+  end
+end
