f692337965f94324993767b9869b4b41ae2bcd81
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-04-23 01:02:27 +0530

    added filetypes for assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Emr::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.precompile += %w( *.css *.js)
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
