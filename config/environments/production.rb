e4cab6ca7095433db6920d4187aed2c7eafebaf1
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-04-24 00:25:21 +0530

    setting temporary settings for devisemailer on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,21 @@ Emr::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+  # Options for action mailer
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "genauth.com",
+      :authentication => :login,
+      :user_name => 'genauth.pvt.ltd@gmail.com',
+      :password  => 'htuaneg123' }
+
+  config.mailer = 'DeviseMailer'
+
+  
 end
