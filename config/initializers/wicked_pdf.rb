25d67fef236ced9bfbfb4f45fe4e09e87eddc5f4
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-05-19 13:03:55 +0530

    added wkhtmltopdf binaries

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,10 @@
-WickedPdf.config = {
-	:exe_path => '/usr/bin/wkhtmltopdf'
-}
+# WickedPdf.config = {
+# 	:exe_path => '/usr/bin/wkhtmltopdf'
+# }
+
+if Rails.env.staging? || Rails.env.production?
+  exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+else
+  exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
+  # exe_path = '/usr/local/bin/wkhtmltopdf'
+end
