e154a72f238f7ee007ee12f08e054a105ef4c819
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-05-20 18:46:11 +0530

    NAD button in Examination

--- app/assets/javascripts/examinations.js
+++ app/assets/javascripts/examinations.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function setNad(elem_id){
+	elem = document.getElementById(elem_id)
+	elem.value = "No Abnormality Detected"
+}
