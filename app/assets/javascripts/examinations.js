151d47fb7fc03e783ea63e5814ae0be542f75f46
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-05-21 18:13:43 +0530

    NAD value in config.yml

--- app/assets/javascripts/examinations.js
+++ app/assets/javascripts/examinations.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-function setNad(elem_id){
+function setNad(elem_id,text){
 	elem = document.getElementById(elem_id)
-	elem.value = "No Abnormality Detected"
+	elem.value = text
 }
