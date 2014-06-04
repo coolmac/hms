b33e982b03c2e74332842c971a15276ce77b4141
Mahesh Ghadage <mailmemahesh91@gmail.com>
2014-06-04 13:16:14 +0530

    Records deleted from DB if the value is nil

--- app/controllers/visit_descriptive_questions_controller.rb
+++ app/controllers/visit_descriptive_questions_controller.rb
@@ -2,29 +2,46 @@ class VisitDescriptiveQuestionsController < ApplicationController
   before_filter :set_patient, :set_visit
 
   def update
-    if params[:id]=='new'
+    binding.pry
+    # if params[:id]=='new' 
       @visit_descriptive_question = VisitDescriptiveQuestion.find_by_visit_id_and_descriptive_question_id(@current_visit.id,params[:qn_id])
       if @visit_descriptive_question.blank?
         @visit_descriptive_question = VisitDescriptiveQuestion.new
       end
-      @visit_descriptive_question.visit_id = @current_visit.id
-      @visit_descriptive_question.descriptive_question_id = params[:qn_id]
-      @visit_descriptive_question.answer = params[:visit_descriptive_question][:answer]
-      respond_to do |format|
-        if @visit_descriptive_question.save
-          # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
+      if params[:visit_descriptive_question][:answer] != ""
+        @visit_descriptive_question.visit_id = @current_visit.id
+        @visit_descriptive_question.descriptive_question_id = params[:qn_id]
+        @visit_descriptive_question.answer = params[:visit_descriptive_question][:answer]
+        respond_to do |format|
+          if @visit_descriptive_question.save
+            # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
+            format.json { respond_with_bip(@visit_descriptive_question) }
+          end
+        end
+      else
+        respond_to do |format|
+          @visit_descriptive_question.destroy
+          @visit_descriptive_question = VisitDescriptiveQuestion.new
           format.json { respond_with_bip(@visit_descriptive_question) }
         end
       end
-    else  
-      @visit_descriptive_question = VisitDescriptiveQuestion.find(params[:id])
-      respond_to do |format|
-        if @visit_descriptive_question.update_attributes(params[:visit_descriptive_question])
-          # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
-          format.json { respond_with_bip(@visit_descriptive_question) }
-        end
-      end
-    end
+    # else  
+    #   @visit_descriptive_question = VisitDescriptiveQuestion.find(params[:id])
+    #   if @visit_descriptive_question.blank?
+    #     @visit_descriptive_question = VisitDescriptiveQuestion.new
+    #   end
+    #   respond_to do |format|
+    #     if params[:visit_descriptive_question][:answer] != ""
+    #       @visit_descriptive_question.update_attributes(params[:visit_descriptive_question])
+    #       # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
+    #       format.json { respond_with_bip(@visit_descriptive_question) }
+    #     else
+    #       @visit_descriptive_question.destroy
+    #       @visit_descriptive_question = VisitDescriptiveQuestion.new
+    #       format.json { respond_with_bip(@visit_descriptive_question) }
+    #     end
+    #   end
+    # end
   end
 
 end
--- app/controllers/visit_investigations_controller.rb
+++ app/controllers/visit_investigations_controller.rb
@@ -2,29 +2,37 @@ class VisitInvestigationsController < ApplicationController
   before_filter :set_patient, :set_visit
 
   def update
-    if params[:id]=='new'
+    # if params[:id]=='new'
       @visit_investigation = VisitInvestigation.find_by_visit_id_and_investigation_id(@current_visit.id,params[:qn_id])
       if @visit_investigation.blank?
         @visit_investigation = VisitInvestigation.new
       end
-      @visit_investigation.visit_id = @current_visit.id
-      @visit_investigation.investigation_id = params[:qn_id]
-      @visit_investigation.report = params[:visit_investigation][:report]
-      respond_to do |format|
-        if @visit_investigation.save
-          # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
+      if params[:visit_investigation][:report] != ""
+        @visit_investigation.visit_id = @current_visit.id
+        @visit_investigation.investigation_id = params[:qn_id]
+        @visit_investigation.report = params[:visit_investigation][:report]
+        respond_to do |format|
+          if @visit_investigation.save
+            # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
+            format.json { respond_with_bip(@visit_investigation) }
+          end
+        end
+      else
+        respond_to do |format|
+          @visit_investigation.destroy
+          @visit_investigation = VisitInvestigation.new
           format.json { respond_with_bip(@visit_investigation) }
         end
       end
-    else 
-      @visit_investigation = VisitInvestigation.find(params[:id])
-      respond_to do |format|
-        if @visit_investigation.update_attributes(params[:visit_investigation])
-          # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
-          format.json { respond_with_bip(@visit_investigation) }
-        end
-      end
-    end
+    # else 
+    #   @visit_investigation = VisitInvestigation.find(params[:id])
+    #   respond_to do |format|
+    #     if @visit_investigation.update_attributes(params[:visit_investigation])
+    #       # format.html {redirect_to(edit_favourite_prescription_path(@favourite_prescription), :notice => "Medicine updated successfully")}
+    #       format.json { respond_with_bip(@visit_investigation) }
+    #     end
+    #   end
+    # end
   end
 
 end
